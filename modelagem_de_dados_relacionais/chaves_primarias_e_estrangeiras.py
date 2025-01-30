d464525e2085d35eca51b9d0cdfd374993414afd
Aiko <mutantetrakor82@gmail.com>
2025-01-30 00:10:00 -0300

    fim aula 3

---
+++ modelagem_de_dados_relacionais/chaves_primarias_e_estrangeiras.py
@@ -0,0 +1,17 @@
+"""
+Chaves primarias
+
+identifica exclusivamente 
+nao pode conter valores nulos (null)
+uma tabela pode ter apenas uma chave primaria 
+
+CREAT TABLE {{tabela}}
+(ID PRIMARY KEY AUTOINCREMENT,
+...)
+ALTER TABLE {{tabela}}
+MODIFY COLUMN ID INT PRIMARY KEY;
+
+
+
+
+"""
