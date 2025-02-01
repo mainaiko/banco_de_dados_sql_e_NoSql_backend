419bbc519f0aba18ee7beb462c98fd9412a95326
Aiko <mutantetrakor82@gmail.com>
2025-02-01 15:36:59 -0300

    aula 5 inicio

---
+++ consultas_avancadas/funcoes_agregadas_e_agrupamento_de_resultados.py
@@ -0,0 +1,14 @@
+"""
+Funçoes agregadas
+COUNT conta o numero de registros
+SUM soma os valores de uma coluna numerica
+AVG calcula a medida dos valores de uma coluna numerica
+MIN retorna o valor minimo de uma coluna
+MAX retorna o valor maximo de uma coluna
+
+
+
+
+
+
+"""
