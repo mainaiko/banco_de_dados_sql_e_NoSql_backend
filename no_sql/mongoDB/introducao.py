e1b2dbb7c3fa159d20b1fcaf52052f43c0fd40d3
Aiko <mutantetrakor82@gmail.com>
2025-02-08 14:33:31 -0300

    modelagem de dados mongo

---
+++ no_sql/mongoDB/introducao.py
@@ -0,0 +1,28 @@
+"""
+banco de dados noSQL orientado a documentos
+grandes volumes de dados, escalabilidade horizontal e modelagem flexivel
+nao exige um esquema
+permite que os documentos sejam armazenados em formado BSON(binary JSON), proporcionando uma
+estrutura semiestruturada
+
+vantagens:
+flexibilidade na modelagem de dados
+escalabilidade horizontal para lidar com grandes volumes de dados
+consultas ricas e suporte a consultas complexas
+alta disponibilizade e tolerancia a falhas
+comunidade ativa e recursos de suporte
+
+desvantagens:
+menor consistencia imediata em comparaçao com bancos de dados relacionais
+consultas complexas podem exigir um maior conhecimento e planejamento adequado
+maior consumo de espaço de armazenamento em comparaçao com banco de dados relacional devido
+a flexibilidade dos documentos
+
+onde o mongodb é usado
+aplicaçoes web: onde a flexibilidade e escalabilidade sao cruciais para lidar com volumes variaveis de dados
+analise de big data: analise de grandes volumes de dados nao estruturados ou semiestruturados, fornecendo uma plataforma
+para armazenar e processar esses dados
+armazenamento de dados semiestruturados: permite a inserçao de documentos com estruturas diferentes em uma mesma coleçao
+casos de uso de geolocalizaçao: com suas funcionalidades de consulta geoespacial, é adeqaudo para casos de uso que envolvem dados baseados
+em localizaçao, como aplicativos de mapeamento e rastreamento
+"""
---
+++ no_sql/mongoDB/modelagem_de_dados_usando_documentos.py
@@ -0,0 +1,44 @@
+"""
+estrutura do mongodb
+
+data_base
+    coleçao(documentos)
+    coleçao2(documentos)
+
+coleçoes:
+agrupamento logico de documentos
+nao exige esquema ou que os documentos tenham a mesma estrutura
+    caracteristicas:
+    devem começar com uma letra ou um underscore(_)
+    podem conter letras numeros e underscores
+    nao podem ser vazios
+    nao podem ter mais de 64bytes de comprimento
+
+documentos:
+sao armazenados em documentos BSON(binary JSON) que sao estruturas flexiveis e semiestruturadas
+cada documento possui um identificador unico chamado "_id"
+é composto por pares de chave e valores
+    caracteriticas:
+    tamanho maximo de no maximo 16mb
+    aninhammento de documentos
+    flexibilidade na evoluçao do esquema
+    tipos de dados simples:
+    string
+    number
+    boolean
+    date
+    null
+    objectld
+    tipos de dados complexos:
+    array
+    documento embutido
+    referencia
+    geoJSON
+
+    
+
+
+
+
+
+"""
