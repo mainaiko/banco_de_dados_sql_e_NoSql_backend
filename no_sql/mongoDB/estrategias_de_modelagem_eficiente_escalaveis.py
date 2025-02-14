f8e2521a0c6a84fa426dd7233552aeaee38be292
Aiko <mutantetrakor82@gmail.com>
2025-02-14 13:00:53 -0300

    atualizacao

--- no_sql/mongoDB/estrategias_de_modelagem_eficiente_escalaveis.py
+++ no_sql/mongoDB/estrategias_de_modelagem_eficiente_escalaveis.py
@@ -5,9 +5,12 @@ a modelagem de dados no mongod deve ser orientada pelas consultas que serao real
 Inner documents
 no mongodb, é comum denormalizar os dados para evitar operaçoes de junçao (join) custosas. isso significa que os
 dados relacionados podem ser armazenados juntos em um unico documento, em vez de serem distribuidos em varias coleçoes
-"""
-
-"""
+    quando usar
+    os dados aninhados sao especificos para o documento pai
+    os dados aninhados sao sempre acessados juntamente com o documento pai
+    a cardinalidade do realacionamento é um-para-muitos(um usuario pode ter varias reservas)
+    quando nao usar
+    se os dados aninhados precisarem ser consultados 
 
 
 """
