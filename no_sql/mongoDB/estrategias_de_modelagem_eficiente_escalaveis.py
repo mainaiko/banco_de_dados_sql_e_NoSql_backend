198f1481352291e0392dffecd276cd2f3bd45292
Aiko <mutantetrakor82@gmail.com>
2025-02-14 12:01:42 -0300

    atualizaçao

---
+++ no_sql/mongoDB/estrategias_de_modelagem_eficiente_escalaveis.py
@@ -0,0 +1,13 @@
+"""
+Modelagem orientada por consultas
+a modelagem de dados no mongod deve ser orientada pelas consultas que serao realizadas com mais frequencia
+
+Inner documents
+no mongodb, é comum denormalizar os dados para evitar operaçoes de junçao (join) custosas. isso significa que os
+dados relacionados podem ser armazenados juntos em um unico documento, em vez de serem distribuidos em varias coleçoes
+"""
+
+"""
+
+
+"""
