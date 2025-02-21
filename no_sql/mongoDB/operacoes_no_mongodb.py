eba186cd21ed728d4575c5c2e551640ccd913709
Aiko <mutantetrakor82@gmail.com>
2025-02-21 10:59:27 -0300

    continuacao mongodb

--- no_sql/mongoDB/operacoes_no_mongodb.py
+++ no_sql/mongoDB/operacoes_no_mongodb.py
@@ -1,4 +1,18 @@
 """
+Criando um dataBase
+use{{nome_do_banco}}
+
+enquanto o database nao tiver uma collection ele nao sera apresentado na lista
+
+Criando uma collection
+
+db.createCollection("usuarios")
+db.createCollection("destinos")
+
+Inserindo documentos
+
+db.usuarios.insertOne({});
+db.usuarios.insertMany([{}]);
 
 
 """
