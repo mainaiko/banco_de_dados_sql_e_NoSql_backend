5e142a162a6006ef7229c57bc3b10fead32d4dfe
Aiko <mutantetrakor82@gmail.com>
2025-03-04 11:46:54 -0300

    introducao

---
+++ introducao_bando_de_dados_python/part_1.py
@@ -0,0 +1,24 @@
+"""
+onjetivo geral
+vamos abordar os conceitos basicos de banco de dados e como podemos interagir com eles usando a DB API em python
+
+o que é um bando de dados
+coleçoes organizada de dados armazenados e acessados eletronicamente a partir de um sistema
+
+papel do SGBD
+os sistemas de gerenciamento de bando de dados (SGBD) sao softwares que interagem com o usuario, outras aplicaçoes
+e o proprio banco de dados para capturar e analisar os dados. Existem muitos SGBDs disponiveis no mercado
+ex: MySQL, SQLite, Oracle Database, MariaDB.
+
+tabelas
+em um banco de dados relacional, tabela é uma estrutura que organiza os dados em linhas e colunas semelhante a uma planilha
+cada linha representa um registro distinto e cada coluna representa um tipo de informaçao
+ex: uma tabela "clientes" pode ter campos como "ID", "nome", "email" e "telefone"
+
+chaves primarias
+cada tabela em um banco de dados relacional deve ter uma chave primaria, que é uma coluna ou conjunto de colunas cujo o valor
+é unico para cada registro. isso garante que cada registro na tabela possa ser identificado de maneira unica
+
+
+
+"""
